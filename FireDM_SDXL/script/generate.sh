6612bccc8fdaf1907355086fae432a793beebb85
HongTao Zheng <138377712+ZhengHongtao2001@users.noreply.github.com>
2024-12-17 01:37:55 +0800

    Update generate.sh

--- FireDM_SDXL/script/generate.sh
+++ FireDM_SDXL/script/generate.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python generate.py --grounding_ckpt './checkpoint/Train/latest_checkpoint.pth' --n_each_class 1000 --outdir './Results/image_mask/' --thread_num 1 --H 512 --W 512 --config ./config/VOC/Sematic.yaml
+CUDA_VISIBLE_DEVICES=5 python generate.py --grounding_ckpt 'checkpoint/Train_10_images_t1_attention_transformer_VOC_10layers_ablation_200images/best_checkpoint.pth' --n_each_class 1000 --outdir 'generated_data/test' --thread_num 1 --H 1024 --W 1024 --config ./config/VOC/VOC_Sematic_Seg_5Images.yaml
