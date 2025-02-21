485a4d8b2a81a8cf0582c526be60ce2b29c803aa
HongTao Zheng <138377712+ZhengHongtao2001@users.noreply.github.com>
2025-02-21 20:03:40 +0800

    Update train.sh

--- FireDM_SDXL/script/train.sh
+++ FireDM_SDXL/script/train.sh
@@ -2,5 +2,5 @@
 # CUDA_LAUNCH_BLOCKING=4 
 # CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=1 python tools/train_semantic_voc.py --save_name Train_5_images_t1_attention_transformer_VOC_10layers --config ./config/VOC/VOC_Sematic_Seg_5Images.yaml --image_limitation 5
 
-# ablation study
+# Train
 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=2 python train.py --save_name Train_10_images_t1_attention_transformer_VOC_10layers_ablation_200images --config ./config/VOC/VOC_Sematic_Seg_5Images.yaml --image_limitation 118
